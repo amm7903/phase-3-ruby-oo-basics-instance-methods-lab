d8027162f65a9dcde551d48b7e6155e37805e012
Alejandro Marcano <+>
2021-11-02 16:49:32 -0400

    completed assignment

---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+    def talk 
+        puts "Hello World!"
+    end
+    def walk 
+        puts "The person is walking"
+    end
+end
+
+alejandro = Person.new
+alejandro.talk
+alejandro.walk
